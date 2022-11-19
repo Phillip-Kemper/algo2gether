b4095877cc2b07f70078ec4d932eeaa249d1f525
phillip-kemper <phillip-kemper@gmx.net>
2022-11-19 10:24:58 +0100

    adapt layout, create admin page

--- client/dashboard/pages/_app.tsx
+++ client/dashboard/pages/_app.tsx
@@ -1,8 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Algo2Gether</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        <Component {...pageProps} />
+      </main>
+
+      <footer className="flex h-24 w-full items-center justify-center border-t">
+        <a
+          className="flex items-center justify-center gap-2"
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Built for HackaTUM 2022
+        </a>
+      </footer>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- client/dashboard/pages/index.tsx
+++ client/dashboard/pages/index.tsx
@@ -3,29 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 
 const Home: NextPage = () => {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Algo2Gether</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <p>Hi</p>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Built for HackaTUM 2022
-        </a>
-      </footer>
-    </div>
-  );
+  return <p>Hi</p>;
 };
 
 export default Home;
