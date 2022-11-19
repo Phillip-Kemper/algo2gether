b72ad265c17a3096b1041a6344013e23ed28c49f
phillip-kemper <phillip-kemper@gmx.net>
2022-11-19 10:09:51 +0100

    create algo utils

---
+++ client/dashboard/src/utils/constants.js
@@ -0,0 +1,25 @@
+import algosdk from "algosdk";
+import MyAlgoConnect from "@randlabs/myalgo-connect";
+
+const config = {
+  algodToken: "",
+  algodServer: "https://node.testnet.algoexplorerapi.io",
+  algodPort: "",
+  indexerToken: "",
+  indexerServer: "https://algoindexer.testnet.algoexplorerapi.io",
+  indexerPort: "",
+};
+
+export const algodClient = new algosdk.Algodv2(
+  config.algodToken,
+  config.algodServer,
+  config.algodPort
+);
+
+export const indexerClient = new algosdk.Indexer(
+  config.indexerToken,
+  config.indexerServer,
+  config.indexerPort
+);
+
+export const myAlgoConnect = new MyAlgoConnect();
