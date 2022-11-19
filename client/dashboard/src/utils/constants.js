0b7fb429dfcec881b4268ccfab8df6b29e5ef367
phillip-kemper <phillip-kemper@gmx.net>
2022-11-19 10:11:26 +0100

    more utils

--- client/dashboard/src/utils/constants.js
+++ client/dashboard/src/utils/constants.js
@@ -23,3 +23,11 @@ export const indexerClient = new algosdk.Indexer(
 );
 
 export const myAlgoConnect = new MyAlgoConnect();
+
+export const base64ToUTF8String = (base64String) => {
+  return Buffer.from(base64String, "base64").toString("utf-8");
+};
+
+export const utf8ToBase64String = (utf8String) => {
+  return Buffer.from(utf8String, "utf8").toString("base64");
+};
