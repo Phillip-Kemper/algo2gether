8cf485a6d53464bb3b7beb8380e707c3b767fd0a
phillip-kemper <phillip-kemper@gmx.net>
2022-11-20 09:56:37 +0100

    use actual address

--- client/dashboard/pages/index.tsx
+++ client/dashboard/pages/index.tsx
@@ -76,7 +76,7 @@ const Home: NextPage = () => {
   return (
     <>
       <Typography className="m-5 absolute top-0 right-3">
-        Address: XXRBLKSVRI5UDNVFCUZMQ4J...
+        Address: {address}
       </Typography>
       <Typography variant="h2">Welcome to TBC</Typography>
       <Typography variant="h4"> Your current status:</Typography>
